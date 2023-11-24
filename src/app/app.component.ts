3456a40a02a49549501a4886781af193294d9e06
alt0828 <alt0828alt0828@gmail.com>
2023-11-24 14:51:52 +0800

    feat(todo):設定待辦事項輸出型屬性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,8 @@ import { TodoComponent } from './todo/todo.component';
 export class AppComponent {
   content = '待辦事項 A';
   hasFinished = false;
+
+  onSetStatus(hasFinished: boolean): void {
+    this.hasFinished = hasFinished;
+  }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,8 @@
 import {
   Component,
+  EventEmitter,
   Input,
+  Output,
   booleanAttribute,
   numberAttribute,
 } from '@angular/core';
@@ -20,7 +22,9 @@ export class TodoComponent {
   content!: string;
   @Input({ transform: booleanAttribute })
   hasFinished!: boolean;
+  @Output()
+  readonly hasFinishedChange = new EventEmitter();
   onSetStatus(hasFinished: boolean): void {
-    this.hasFinished = hasFinished;
+    this.hasFinishedChange.emit(hasFinished);
   }
 }
