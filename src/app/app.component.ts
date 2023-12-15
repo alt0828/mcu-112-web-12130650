a4935b9260667174a8f0136d5f43f5b0f8757558
alt0828 <alt0828alt0828@gmail.com>
2023-12-15 15:45:17 +0800

    feat(task): 實作刪除資料庫待辦事項資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,7 @@ export class AppComponent implements OnInit {
     this.taskService.add('待辦事項 C').subscribe(() => this.refresh$.next());
   }
   onRemove(id: number): void {
-    this.taskService.remove(id);
+    this.taskService.remove(id).subscribe(() => this.refresh$.next());
   }
 
   onStateChange({ task, state }: { task: Todo; state: boolean }): void {
