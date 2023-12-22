6c7d88af74ea936dbc45f202ca32e03915ad978c
alt0828 <alt0828alt0828@gmail.com>
2023-12-22 16:33:24 +0800

    feat(todo-form): 待辦事項內容必填驗證實作

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,12 +1,17 @@
 import { Component, EventEmitter, HostBinding, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 import { ITodoForm } from '../interface/todo-form.interface';
 import { Todo } from '../model/todo';
 @Component({
   selector: 'app-todo-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css',
 })
@@ -16,7 +21,9 @@ export class TodoFormComponent {
   @Output()
   readonly save = new EventEmitter<Todo>();
   readonly form = new FormGroup<ITodoForm>({
-    content: new FormControl<string | null>(null),
+    content: new FormControl<string | null>(null, {
+      validators: [Validators.required],
+    }),
   });
   get formData(): Todo {
     return new Todo({
