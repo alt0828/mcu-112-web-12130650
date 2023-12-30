9341b351a4dc9c431439a314174db7e311a091b8
alt0828 <alt0828alt0828@gmail.com>
2023-12-30 16:49:13 +0800

    feat(router): 切換登入與註冊頁面實作

---
+++ src/app/register-page/register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css',
+})
+export class RegisterPageComponent {}
