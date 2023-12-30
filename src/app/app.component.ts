9f4fdc35b09d1c06555d0c3ad806baddfbd8d5b8
alt0828 <alt0828alt0828@gmail.com>
2023-12-30 16:44:53 +0800

    feat(home): 待辦事項頁面路由組態設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
